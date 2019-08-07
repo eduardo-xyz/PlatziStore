a840e41238a910146355a9c89df1a812fd1c4a77
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2019-08-07 01:41:28 -0500

    unitTest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { AddModule } from './add/add.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AddModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ProductsComponent
       ],
     }).compileComponents();
   }));
@@ -20,16 +24,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'platzi-store'`, () => {
+  it(`should have as title 'Tienda Platzi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('platzi-store');
+    expect(app.title).toEqual('Tienda Platzi');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to platzi-store!');
+    expect(compiled.querySelector('h1').textContent).toContain('Bienvenido a Tienda Platzi');
   });
 });
--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -22,4 +22,17 @@ describe('ProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in an h2 tag', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Lista de productos');
+  });
+
+  it('should filter the products', () => {
+    component.filter('a');
+    expect(component.products).toEqual([
+      {id: 1, name: 'Camisa', price: 16.99},
+      {id: 2, name: 'Taza', price: 5.99}
+    ]);
+  });
 });
